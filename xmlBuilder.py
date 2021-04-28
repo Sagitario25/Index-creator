16365ed441d96b8344f79a3fb62133065abc5636
Sagitario25 <adanielsanchezgarcia@gmail.com>
2021-04-28 21:26:42 +0200

    Nomenclature problem XML

--- xmlBuilder.py
+++ xmlBuilder.py
@@ -2,15 +2,15 @@ import xml.etree.cElementTree as ET
 
 def generate (data):
 	root = ET.Element ("root")
-	feature = ET.SubElement (root, "feature")
-	features = ET.SubElement (feature, "list")
+	features = ET.SubElement (root, "features")
+	featureList = ET.SubElement (features, "list")
 
-	features.text = str ([i for i in data])
+	featureList.text = str ([i for i in data])
 
 	for i in data:
-		feature.append (featureXML (str (i), data [i]))
+		features.append (featureXML (str (i), data [i]))
 
-	return ET.tostring (feature, encoding = "UTF-8", method = "xml").decode ()
+	return ET.tostring (features, encoding = "UTF-8", method = "xml").decode ()
 
 def featureXML (name, data):
 	fRoot = ET.Element ("root")
